33babefc47fc6cb3f7f88825aeacc0c9a205f23d
Dougal Stewart <dougal.koji@gmail.com>
2019-01-31 20:55:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sorting Algorithms
+
+List of included algorithms:
---
+++ sort.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iterator>
+#include <type_traits>
+
+template<typename T, typename = void>
+struct is_iterator {
+   static constexpr bool value = false;
+};
+
+template<typename T>
+struct is_iterator<T, typename std::enable_if<!std::is_same<typename std::iterator_traits<T>::value_type, void>::value>::type> {
+   static constexpr bool value = true;
+};
+
+#include "sort.tpp"
